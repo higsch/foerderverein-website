8a3554f9d1e49b80baba068cf31dc372547f0d5f
higsch <matthias@higsch.com>
2021-09-05 20:42:58 +0200

    Neuigkeiten page

---
+++ frontend/src/routes/neuigkeiten.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+  import { getPosts } from '$lib/utils/api';
+  
+  export async function load({ fetch }) {
+		const posts = await(getPosts({fetch}));
+		return {
+			props: {
+				posts
+			}
+		};
+	}
+</script>
+
+<script>
+	import PostCard from '$lib/components/PostCard/index.svelte';
+
+	export let posts;
+</script>
+
+<section class="post-cards">
+	{#each posts as post (post.id)}
+		<PostCard post={post} />
+	{/each}
+</section>
+
+<style>
+	
+</style>
