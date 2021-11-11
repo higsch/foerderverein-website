1e0821846b800d65ce13edfcbbcdb2dd9963ebaf
higsch <matthias@higsch.com>
2021-11-11 08:43:42 +0100

    Download card reacts completely on link

--- frontend/src/lib/components/DownloadCard/index.svelte
+++ frontend/src/lib/components/DownloadCard/index.svelte
@@ -3,7 +3,11 @@
 </script>
 
 <div class="download-card">
-  <div class="content">
+  <a
+    class="content"
+    href={download.file.url}
+    target="_blank"
+  >
     <svg
       width="30"
       xmlns="http://www.w3.org/2000/svg"
@@ -15,16 +19,10 @@
         d="M369.9 97.9L286 14C277 5 264.8-.1 252.1-.1H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V131.9c0-12.7-5.1-25-14.1-34zM332.1 128H256V51.9l76.1 76.1zM48 464V48h160v104c0 13.3 10.7 24 24 24h104v288H48z"
       />
     </svg>
-    <a
-      class="download-link"
-      href={download.file.url}
-      target="_blank"
-    >
       <h2>
         {download.name}
       </h2>
     </a>
-  </div>
   <div class="foot-bar"></div>
 </div>
 
@@ -49,10 +47,6 @@
     border: none;
     border-top-left-radius: var(--border-radius);
     border-top-right-radius: var(--border-radius);
-  }
-
-  a.download-link {
-    border: none;
   }
 
   h2 {
